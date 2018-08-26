191e90c42259b4b15dd000b0ca91adf9d113d445
Nick Pasc <nickpasc90@gmail.com>
2018-08-26 11:31:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var newArray = array
-  return newArray.unshift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element){
