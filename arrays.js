84fd6d7469ae58093b4caadcd4986b946eeb88c6
Nick Pasc <nickpasc90@gmail.com>
2018-08-26 11:01:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,15 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  return [...array, element]
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.push(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element){
-  var newArray = array.slice(array.length - 1)
-  return newArray
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element){
   return array.slice(array.length - 1)
