737ec4d7e35970e2ddb5960720fdbc0a5a106fcc
Nick Pasc <nickpasc90@gmail.com>
2018-08-26 11:13:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,5 +26,5 @@ function removeElementFromBeginningOfArray(array, element){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element){
-  return array.
+  return array.slice(1)
 }
